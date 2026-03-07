4790f8da3930ca3a9f2a7d3c2186fa17c9979a43
opt <opt@chipmunk.land>
2026-03-07 17:58:03 +0000

    Target new version of messaging-lib

--- src/main/java/dev/optimistic/chatattestation/MessagingEntrypointImpl.java
+++ src/main/java/dev/optimistic/chatattestation/MessagingEntrypointImpl.java
@@ -2,9 +2,9 @@ package dev.optimistic.chatattestation;
 
 import dev.optimistic.chatattestation.crypto.SigningManager;
 import io.netty.buffer.ByteBuf;
-import land.chipmunk.code.kaboomstandardsorganization.messaginglib.common.channel.ChannelPayloadReceiver;
-import land.chipmunk.code.kaboomstandardsorganization.messaginglib.common.channel.Messenger;
 import land.chipmunk.code.kaboomstandardsorganization.messaginglib.fabric.FabricMessagingEntrypoint;
+import land.chipmunk.code.kaboomstandardsorganization.messaginglib.fabric.FabricMessenger;
+import land.chipmunk.code.kaboomstandardsorganization.messaginglib.fabric.FabricPayloadReceiver;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.jodah.expiringmap.ExpiringMap;
 import net.minecraft.util.Util;
@@ -14,16 +14,16 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-public final class MessagingEntrypointImpl implements FabricMessagingEntrypoint, ChannelPayloadReceiver<ClientPlayNetworking.Context> {
+public final class MessagingEntrypointImpl implements FabricMessagingEntrypoint, FabricPayloadReceiver {
   public static final String CHANNEL_NAME = "chat-attestation/v1:stream";
   public static final Map<StreamCacheKey, ByteBuf> PAYLOAD_MAP = ExpiringMap.builder()
     .maxSize(1024)
     .expiration(1, TimeUnit.MINUTES)
     .build();
-  public static volatile Messenger<ClientPlayNetworking.Context> MESSENGER_INSTANCE;
+  public static volatile FabricMessenger MESSENGER_INSTANCE;
 
   @Override
-  public void onRegistrationAvailable(@NotNull Messenger<ClientPlayNetworking.Context> messenger) {
+  public void onRegistrationAvailable(@NotNull FabricMessenger messenger) {
     MESSENGER_INSTANCE = messenger;
 
     MESSENGER_INSTANCE.receivePayloads(CHANNEL_NAME, this);
