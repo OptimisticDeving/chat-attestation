67f2aed56d52efd50fce573404442bd45fbdfffc
opt <opt@chipmunk.land>
2026-07-05 23:29:39 +0000

    Port to 26.2

--- fabric/src/main/java/dev/optimistic/chatattestation/mixin/ChatListenerMixin.java
+++ fabric/src/main/java/dev/optimistic/chatattestation/mixin/ChatListenerMixin.java
@@ -78,7 +78,7 @@ public abstract class ChatListenerMixin {
   @Unique
   private static GuiMessageTag createTag(ChatFormatting color, String description) {
     return new GuiMessageTag(
-      Objects.requireNonNull(color.getColor()),
+      Objects.requireNonNull(TextColor.fromLegacyFormat(color)).getValue(),
       null,
       Component.literal(description),
       "chat-attestation"
@@ -121,7 +121,7 @@ public abstract class ChatListenerMixin {
     GuiMessageTag newTag
   ) {
     this.minecraft.schedule(() -> {
-      final var chatComponent = this.minecraft.gui.getChat();
+      final var chatComponent = this.minecraft.gui.hud.getChat();
       final var chatComponentDuck = (ChatComponentDuck) chatComponent;
       final var chatComponentAccessor = (ChatComponentAccessor) chatComponent;
       final var allMessages = chatComponentAccessor.getAllMessages();
